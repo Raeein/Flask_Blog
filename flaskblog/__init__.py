b0da86abc9aac90f2a2473738997d5c63613099e
Raeein Bagheri <raeein@aol.com>
2022-02-10 20:55:24 -0500

    Upload files

---
+++ flaskblog/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "e67653aa7298adcdc002cb1772507489"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+login_manager.login_message_category = "info"
+
+
+from flaskblog import route
